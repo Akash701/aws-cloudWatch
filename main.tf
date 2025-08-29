e24516f7b7f441ffc13b8e0c27767fd717f7b0e8
Akash <akashjnair701@gmail.com>
2025-08-28 19:05:36 -0600

    WIP before history rewrite

--- main.tf
+++ main.tf
@@ -39,23 +39,61 @@ resource "aws_cloudwatch_log_metric_filter" "error_filter" {
   
 # }
 
+resource "aws_iam_role" "budget_alerts_role" {
+  name = "budget-alerts-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "budgets.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
 // SNS Topic for sending alerts
 resource "aws_sns_topic" "alerts" {
   name = "myapp-alerts"
 }
 
 //SNS Subscription to send email alerts
+resource "aws_iam_policy" "budget_alerts_policy" {
+  name        = "budget-alerts-policy"
+  description = "Allow AWS Budgets to publish to SNS"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "sns:Publish"
+        Resource = aws_sns_topic.alerts.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "budget_alerts_attach" {
+  role       = aws_iam_role.budget_alerts_role.name
+  policy_arn = aws_iam_policy.budget_alerts_policy.arn
+}
 
 resource "aws_sns_topic_subscription" "email_sub" {
   topic_arn = aws_sns_topic.alerts.arn
   protocol  = "email"
-  endpoint  = "akashjnair701@gmail.com"
+  endpoint  = var.endpoint_email
 }
 
 resource "aws_sns_topic_subscription" "number_sub" {
   topic_arn = aws_sns_topic.alerts.arn
   protocol  = "sms"
-  endpoint  = "+918888888888"
+  endpoint  = var.endpoint_number
 }
 
 
@@ -75,7 +113,68 @@ resource "aws_cloudwatch_metric_alarm" "error_alarm" {
   
 }
 
+resource "aws_budgets_budget" "monthly_cost" {
+  name         = "monthly-cost-budget"
+  budget_type  = "COST"
+  limit_amount = "5"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
 
+  # cost_filters = {
+  #   name = "Service"
+  #   values = ["AmazonEC2"]
+  # }
+notification {
+  comparison_operator = "GREATER_THAN"
+  threshold = 80
+  threshold_type = "PERCENTAGE"
+  notification_type = "FORECASTED"
+  
+}
+# notification {
+#   comparison_operator = "GREATER_THAN"
+#   threshold = 100
+#   threshold_type = "PERCENTAGE"
+#   notification_type = "ACTUAL"
+#   subscriber {
+#     address = var.endpoint_number
+#     subscription_type = "SMS"
+#   }
+# }
+}
+
+# Budget Action: when actual > 100%
+resource "aws_budgets_budget_action" "budget_alert_action" {
+  budget_name        = aws_budgets_budget.monthly_cost.name
+  action_type        = "APPLY_IAM_POLICY"
+  execution_role_arn = aws_iam_role.budget_alerts_role.arn
+  notification_type  = "FORECASTED"
+  approval_model = "AUTOMATIC"
+
+  action_threshold {
+    action_threshold_type = "PERCENTAGE"
+    action_threshold_value = 80
+  }
+
+  definition {
+    iam_action_definition {
+      policy_arn = aws_iam_policy.budget_alerts_policy.arn
+      roles      = [aws_iam_role.budget_alerts_role.name]
+      groups     = []
+      users      = []
+    }
+  }
+
+   subscriber {
+    subscription_type = "EMAIL"
+    address          = var.endpoint_email
+  }
+
+  # subscriber {
+  #   subscription_type = "SMS"
+  #   address           = aws_sns_topic.alerts.arn
+  # }
+  }
 resource "null_resource" "push_test_error" {
   depends_on = [aws_cloudwatch_log_stream.app_log_stream]
 
--- variables.tf
+++ variables.tf
@@ -6,3 +6,11 @@ variable "log_group_name" {
 variable "retaination_in_days" {
   default     = 7
 }
+
+variable "endpoint_email" {
+  default = "akashjnair701@gmail.com"
+}
+
+variable "endpoint_number" {
+  default = "+13065303864"
+}
